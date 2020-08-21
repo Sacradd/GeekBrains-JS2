4738faf2cf514ff0a716147697b51ed672315c51
Sacradd <Lackar@mail.ru>
2020-08-22 00:38:42 +0300

    Получение данных для списка товаров из файла json, с внешнего ресурса.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,50 @@
 // Добавление товара метод ООП
 
+
+// Ссылка на внешний файл json
+
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+
+
 class ProductList{
     constructor(container='.products'){
         this.container = container;
         this.goods = [];
-        //this.allProducts = [];//массив товаров c добавлением фото
-        this._fetchProducts();
-        this.render();//вывод товаров на страницу
-        this.calcAllGoods();
+        this.allProducts = [];//массив товаров c добавлением фото
+        this._getProducts()
+            .then(data => {
+                this.goods = [...data];
+                this.render()
+            });
+        //this._fetchProducts();
+        // this.render();//вывод товаров на страницу
+        // this.calcAllGoods();
     }
-    _fetchProducts(){
-        this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
-        ];
+    // _fetchProducts(){
+    //     this.goods = [
+    //         {id: 1, title: 'Notebook', price: 2000},
+    //         {id: 2, title: 'Mouse', price: 20},
+    //         {id: 3, title: 'Keyboard', price: 200},
+    //         {id: 4, title: 'Gamepad', price: 50},
+    //     ];
+    // }
+
+    _getProducts(){
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
     }
     
     render(){
         const block = document.querySelector(this.container);
         for(let product of this.goods){
             const item = new ProductItem(product);
-            
-           // this.allProducts.push(item);
-             block.insertAdjacentHTML("beforeend",item.render());
-             //block.innerHTML += item.render();
+            this.allProducts.push(item);
+            block.insertAdjacentHTML("beforeend",item.render());
+            //block.innerHTML += item.render();
         }
     }
 
@@ -43,17 +62,19 @@ class ProductList{
 
 class ProductItem{
     constructor(product,img='https://placehold.it/200x150'){
-        this.title = product.title;
-        this.id = product.id;
+        this.title = product.product_name;
+        this.id = product.id_product;
         this.price = product.price;
         this.img = img;
     }
     render(){
-           return `<div class="product-item">
-                <img src="${this.img}">
-                <h3>${this.title}</h3>
-                <p>${this.price}</p>
-                <button class="buy-btn">Купить</button>
+           return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class = "desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} $</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
             </div>`
     }
 }
